c382bd02cec952d96e6b88213294d801a91221bc
JesperKYH <jesper.pettersson@student.kyh.se>
2021-01-10 14:34:00 +0100

    Nej

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Landingpage from "./components/Landingpage";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import "./style/styles.css";
 import LoveApi from "./components/apis/LoveApi";
@@ -13,20 +13,13 @@ import JokeApi from "./components/apis/JokeApi";
 const App = () => {
   return (
     <>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Landingpage />
 
         <Switch>
           <Route exact path="/love" component={LoveApi} />
           <Route exact path="/generate-fun-fact" component={RandomNumFact} />
           <Route exact path="/have-a-laugh" component={JokeApi} />
-
-          {/* <Route path="gutenberg-api/generate-fun-fact" exact>
-            <RandomNumFact />
-          </Route>
-          <Route path="gutenberg-api/have-a-laugh" exact>
-            <JokeApi />
-          </Route> */}
         </Switch>
       </Router>
     </>
