b56d7f4b1920478bf89544d288d00166f2cecc31
JesperKYH <jesper.pettersson@student.kyh.se>
2021-01-10 14:21:44 +0100

    Bruuuuuh

--- src/App.js
+++ src/App.js
@@ -12,28 +12,29 @@ import {
 } from "react-router-dom";
 
 import "./style/styles.css";
-import JokeApi from "./components/apis/JokeApi";
 import LoveApi from "./components/apis/LoveApi";
 import RandomNumFact from "./components/apis/RandomNumFact";
+import JokeApi from "./components/apis/JokeApi";
+
 // import Route from "./components/Route";
 
 const App = () => {
   return (
     <>
-      <Router basename="https://jesperkyh.github.io">
+      <Router basename="jesperkyh.github.io/">
         <Landingpage />
         <Link to="love"></Link>
         <Link to="generate-fun-fact"></Link>
         <Link to="have-a-laugh"></Link>
 
         <Switch>
-          <Route path="/gutenberg-api/love" exact>
+          <Route path="gutenberg-api/love" exact>
             <LoveApi />
           </Route>
-          <Route path="/gutenberg-api/generate-fun-fact" exact>
+          <Route path="gutenberg-api/generate-fun-fact" exact>
             <RandomNumFact />
           </Route>
-          <Route path="/gutenberg-api/have-a-laugh" exact>
+          <Route path="gutenberg-api/have-a-laugh" exact>
             <JokeApi />
           </Route>
         </Switch>
