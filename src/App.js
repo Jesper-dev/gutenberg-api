a78b7c8ba6e690ce79c576dc1a39039d760b3fce
JesperKYH <jesper.pettersson@student.kyh.se>
2021-01-10 13:56:41 +0100

    Tried new router code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React from "react";
 import LoveApi from "./components/apis/LoveApi";
 import RandomNumFact from "./components/apis/RandomNumFact";
-import Landingpage from './components/Landingpage';
+import Landingpage from "./components/Landingpage";
+import ReactRouter from "react-router";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Link,
+  Switch,
+} from "react-router-dom";
 
-
-import "./style/styles.css"
+import "./style/styles.css";
 import JokeApi from "./components/apis/JokeApi";
-import Route from "./components/Route";
+// import Route from "./components/Route";
 
 const App = () => {
   return (
     <>
-      
-      <Landingpage />
-      <Route path="/love">
-        <LoveApi />
-      </Route>
-      <Route path="/generate-fun-fact">
-        <RandomNumFact />
-      </Route>
-      <Route path="/have-a-laugh">
-        <JokeApi/>
-      </Route>
-      
+      <Router>
+        <Landingpage />
+        <Link to="/love"></Link>
+        <Link to="/generate-fun-fact"></Link>
+        <Link to="/have-a-laugh"></Link>
+
+        <Switch>
+          <Route path="/love">
+            <LoveApi />
+          </Route>
+          <Route path="/generate-fun-fact">
+            <RandomNumFact />
+          </Route>
+          <Route path="/have-a-laugh">
+            <JokeApi />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
