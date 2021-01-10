10b52325d54949374dce14b9b56513d93e08e357
JesperKYH <jesper.pettersson@student.kyh.se>
2021-01-10 14:40:33 +0100

    Test new route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Landingpage from "./components/Landingpage";
-import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
 
 import "./style/styles.css";
 import LoveApi from "./components/apis/LoveApi";
@@ -13,15 +13,16 @@ import JokeApi from "./components/apis/JokeApi";
 const App = () => {
   return (
     <>
-      <Router>
+      <HashRouter basename="/">
         <Landingpage />
+        <Link to="/love"></Link>
+        <Link to="/generate-fun-fact"></Link>
+        <Link to="/have-a-laugh"></Link>
 
-        <Switch>
-          <Route exact path="/love" component={LoveApi} />
-          <Route exact path="/generate-fun-fact" component={RandomNumFact} />
-          <Route exact path="/have-a-laugh" component={JokeApi} />
-        </Switch>
-      </Router>
+        <Route path="/love" component={LoveApi} />
+        <Route path="/generate-fun-fact" component={RandomNumFact} />
+        <Route path="/have-a-laugh" component={JokeApi} />
+      </HashRouter>
     </>
   );
 };
