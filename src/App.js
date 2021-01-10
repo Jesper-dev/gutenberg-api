d853a693a4cc6ec52831f6fe564871dfa1764ce4
JesperKYH <jesper.pettersson@student.kyh.se>
2021-01-10 13:59:18 +0100

    Test again

--- src/App.js
+++ src/App.js
@@ -20,18 +20,18 @@ const App = () => {
     <>
       <Router>
         <Landingpage />
-        <Link to="/love"></Link>
-        <Link to="/generate-fun-fact"></Link>
-        <Link to="/have-a-laugh"></Link>
+        <Link to="/gutenberg-api/love"></Link>
+        <Link to="gutenberg-api/generate-fun-fact"></Link>
+        <Link to="gutenberg-api/have-a-laugh"></Link>
 
         <Switch>
-          <Route path="/love">
+          <Route path="gutenberg-api/love">
             <LoveApi />
           </Route>
-          <Route path="/generate-fun-fact">
+          <Route path="gutenberg-api/generate-fun-fact">
             <RandomNumFact />
           </Route>
-          <Route path="/have-a-laugh">
+          <Route path="gutenberg-api/have-a-laugh">
             <JokeApi />
           </Route>
         </Switch>
